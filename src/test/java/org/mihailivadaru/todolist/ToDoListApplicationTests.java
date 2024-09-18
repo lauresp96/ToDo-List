09baeeccbc3c8240b349fc9abfd9d2cfc114df40
Mihai Livadaru <lauresp96@gmail.com>
2024-09-18 14:57:04 +0200

    New Project

---
+++ src/test/java/org/mihailivadaru/todolist/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mihailivadaru.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
